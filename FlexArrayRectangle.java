d710a416e163c360297e1c3e43bc9a9e9c7ac815
ElectiwirezMusic <electiwirez@saratogachess.org>
2017-11-10 16:09:53 -0800

    Update FlexArrayRectangle.java

--- FlexArrayRectangle.java
+++ FlexArrayRectangle.java
@@ -1,31 +1,37 @@
+//Name: Tushar Shrivastav
+//
+//Period: 2
+//
+//Class Description: implements different methods which manipulates an array of rectangles.
 public class FlexArrayRectangle {
 	
+	//declaring private data members
 	private int mySize;
 	
 	private Rectangle[] myArray;
 	
-	// constructors are missing
-	// array is not initialized
+	//defualt constructor
 	public FlexArrayRectangle(){
 		myArray = new Rectangle[20];
 		mySize = 0;
 	}
+	//accepts a size for an array (contructor)
 	public FlexArrayRectangle(int myArraySize){
 		myArray = new Rectangle[myArraySize];
 		mySize = 0;
 	}
-	
+	//returns the logical length of the array
 	public int getLength(){
 		return mySize;
 	}
-	
+	//returns true if the array is logically empty
 	public boolean isEmpty(){
 		
 		if( mySize == 0)
 			return true;
 		return false;
 	}
-
+	//adds data to the end of the array
 	public void append(Rectangle data){
 		
 		if(mySize >= myArray.length )
@@ -33,10 +39,8 @@ public class FlexArrayRectangle {
 		myArray[mySize] = data;
 		mySize++;
 	}
-	
+	//inserts data at the the specified index
 	public void insert(int index, Rectangle data){
-		if(index < 0)
-			return;
 		if(index>= mySize){
 			append(data);
 			return;
@@ -45,28 +49,27 @@ public class FlexArrayRectangle {
 		myArray[index] = data;
 		
 	}
-	
+	//moves elements forward to keep the elements in order while inserting
 	private void moveElementsFORWARD(int index) {
 		for(int i = mySize - 1; i >= index; i--)
 			myArray[i+1] = myArray[i];
 		mySize++;
 	}
-	
+	//removes the element at the specified index
 	public int discard(int index){
-		if(index < 0)
-			return -999;
 		if(index >= mySize)
 			return -999;
 		moveElementsBACKWARDS(index);
 		return index;
 		
 	}
+	//moves elements backwards so the order can be kept in discard method
 	private void moveElementsBACKWARDS(int index){
 		for(int i = index; i <= 0; i-- )
 			myArray[i-1] = myArray[i];
 		mySize--;
 	}
-	
+	//converts the array into a string that can be outputted to the monitor
 	public String toString(){
 		StringBuilder sb = new StringBuilder() ;
 		
@@ -78,10 +81,11 @@ public class FlexArrayRectangle {
 		return "[" + sb.toString() + "]";
 		
 	}
+	//resizes the array using the other resize method
 	private void resize(){
 		resize(5);
 	}
-	
+	//resizes the array to accommodate for more values
 	private void resize(int grownAmount){
 		Rectangle [] rectArr = new Rectangle [myArray.length + grownAmount];
 		
